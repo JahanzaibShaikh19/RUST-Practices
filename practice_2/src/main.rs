55bc0d74084b104993e6d980bf58f4ae09713a06
JahanzaibShaikh19 <jahanzaibshaikh16@gmail.com>
2024-02-17 17:55:30 +0500

    Shadowing

--- practice_2/src/main.rs
+++ practice_2/src/main.rs
@@ -1,6 +1,36 @@
 fn main() {
+    //  Mutable value changed
+
     let mut x = 15;
     println!("The value of x: {}", x);
     x = 15 + 5;
+    println!("The value of x: {}", x);
+
+    // Without Mutable value Changed
+
+    let x = 15;
+    println!("The value of x: {}", x);
+    let x = x + 1;
+    println!("The value of x: {}", x);
+    let x = x + 1;
+    println!("The value of x: {}", x);
+
+    // Shadowing
+
+    {
+        let x = x + 1;
+        println!("The value of x: {}", x);
+    }
+
+    let x = x + 1;
+    println!("The value of x: {}", x);
+
+    let x = 50;
+    println!("The value of x: {}", x);
+
+    let x = "String";
+    println!("The value of x: {}", x);
+
+    let x = x + "Data Type";
     println!("The value of x: {}", x);
 }
