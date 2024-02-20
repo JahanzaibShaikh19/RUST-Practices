3b8e7981382f211036b870fd8844c4930e55dab8
JahanzaibShaikh19 <jahanzaibshaikh16@gmail.com>
2024-02-20 23:02:16 +0500

    Pass Arguments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,18 @@
 fn main() {
+    let mut x = 10;
     println!("Outer Function");
 
     another_function();
+    string_passed("HelloWorld!", x);
+    x = 20;
+    string_passed("HelloWorld!", x);
 }
 
 fn another_function() {
     println!("Inner function");
+}
+
+fn string_passed(str: &str, len: i32) {
+    println!("String: {}", str);
+    println!("Length: {}", len);
 }
