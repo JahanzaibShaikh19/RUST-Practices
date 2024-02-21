69fe2629523a77354f9b24520424673cbe232cd7
JahanzaibShaikh19 <jahanzaibshaikh16@gmail.com>
2024-02-21 23:20:06 +0500

    OwnerShip/ Clone

--- owner_ship/src/main.rs
+++ owner_ship/src/main.rs
@@ -4,4 +4,16 @@ fn main() {
     s.push_str(", world!"); // push_str() appends a literal to a String
 
     println!("{}", s); // This will print `hello, world!`
+
+    let mut s1 = 5;
+    let s2 = s1;
+    s1 = 8;
+    println!("{}", s2);
+    println!("{}", s1);
+
+    // Clone Deeply copied
+    let s1 = String::from("Tony Stark");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
